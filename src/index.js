ab0a2c75a9a238f5c314a64600cba0945ab2d8cc
attobra <nora.attobra@gmail.com>
2020-10-13 10:57:31 -0400

    add images

--- src/index.js
+++ src/index.js
@@ -2,28 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+const allBooks = [{
+  img : "https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg",
+  title: "If Animals Kissed Good Night",
+  author:"Amelia hepworth"
+},{
+  img: "https://images-na.ssl-images-amazon.com/images/I/91O1RKTpiWL._AC_UL200_SR200,200_.jpg",
+  title: "the office",
+  author:"Robb pearlman"
+}]
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    {allBooks.map(bookItem => <Book 
+        img={bookItem.img} title={bookItem.title} author={bookItem.author}
+      > <button>Buy on Amazon</button>
+       </Book>
+      )}
+      
     </section>
   )
 }
 
-const Book = () =>{
+const Book = (props) =>{
+  const {img, title, author, children} = props
   return (<article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt=""/>
+      {children}
+      <h1>{title}</h1>
+      <h5>{author}</h5>
+      
   </article>)
 }
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg" alt=""/>
 
-const Title = () => <h1>If Animals Kissed Good Night</h1>
-const Author = () => <h5 style={{color:'#617d98', 
-fontSize: '0.75rem', marginTop: '0.25rem'}}>Ann Whitford Paul</h5>
 ReactDOM.render(<BookList />, document.getElementById('root'));
